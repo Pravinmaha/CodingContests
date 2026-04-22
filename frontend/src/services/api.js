17f962e461973d82b9a350fd7645a8a4dc425111
Pravin <pravinmahale028@gmail.com>
2026-04-22 08:31:00 +0530

    fixed api base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,39 @@
-// src/services/api.js
 import axios from 'axios';
 
-const token = localStorage.getItem("token");
-
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // your backend base URL
-  // baseURL: 'https://dcode-backend-h68l.onrender.com/api',
-  withCredentials: true, // required for sending cookies/auth headers
+  baseURL: 'https://codingcontests-backend.onrender.com/api',
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'Authorization': `Bearer ${token}`  
+    'Accept': 'application/json'
   }
 });
 
+// attach token dynamically
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default api;
+
+// // src/services/api.js
+// import axios from 'axios';
+
+// const token = localStorage.getItem("token");
+
+// const api = axios.create({
+//   baseURL: 'https://codingcontests-backend.onrender.com', // your backend base URL
+//   // baseURL: 'https://dcode-backend-h68l.onrender.com/api',
+//   withCredentials: true, // required for sending cookies/auth headers
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Accept': 'application/json',
+//     'Authorization': `Bearer ${token}`  
+//   }
+// });
+
+// export default api;
